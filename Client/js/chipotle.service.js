b0491dba85b08196c1ad13ba09b74f4f07091a92
zdietzen <diezel37@gmail.com>
2015-10-27 15:54:12 -0700

    First Push
    All the basic requirements are finished. Will work on making it look
    better now and add extras

---
+++ Client/js/chipotle.service.js
@@ -0,0 +1,12 @@
+﻿angular.module('app').service('chipotleService', function ($http) {
+    return {
+
+        getRestaurants: function (zip) {
+            var promise = $http.get('https://infinite-depths-3549.herokuapp.com/restaurants/'+zip).then(function (response) {
+               return response.data;
+            });
+
+            return promise;
+        }
+    };
+});
---
+++ Client/js/index.controller.js
@@ -0,0 +1,23 @@
+﻿angular.module('app').controller('indexController', function($scope, $http, chipotleService) {
+
+    $scope.chipotleData = [];
+
+    $scope.map = {
+        center: {
+            latitude: 37,
+            longitude: -103
+        },
+        zoom: 12
+    };
+
+    $scope.getZip = function () {
+        chipotleService.getRestaurants($scope.inputBox).then(function (data) {
+            $scope.chipotleData = data;
+
+            $scope.map.center = {
+                latitude: $scope.chipotleData[0].address.latitude,
+                longitude: $scope.chipotleData[0].address.longitude,
+            };
+        });
+    }
+});
